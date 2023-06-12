fae7b03a1d809e1817fabf02f8a539a096bead00
ClPhillips10 <Lakephillips3232@gmail.com>
2023-06-12 12:55:54 -0500

    Game Order

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>() { "Grand Theft Auto 5", "Call of Duty", "Fortnite", "Minecraft" };
+            var orderedGames = games.OrderByDescending(n => n.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
+            
         }
     }
 }
+
